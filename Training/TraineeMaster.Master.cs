7e94d055a8fe05d4b18228668fc6a6c888d777c5
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-10 11:41:10 +0530

    Protect trainee pages from direct URL access

--- Training/TraineeMaster.Master.cs
+++ Training/TraineeMaster.Master.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Training
 {
@@ -11,54 +6,11 @@ namespace Training
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            string role = Convert.ToString(Session["Role"]);
+            if (Session["EmpID"] == null || !role.Equals("Trainee", StringComparison.OrdinalIgnoreCase))
             {
-                ////if (Session["TraineeID"] == null)
-                ////{
-                ////    // For demo - Remove in production
-                ////    //Session["TraineeID"] = "TRIN002";
-                ////    //Session["TraineeName"] = "Trainee";
-                ////     Response.Redirect("~/Default.aspx");
-                ////}
-
-                ////LoadTrainerInfo();
-            }
-        }
-
-        private void LoadTrainerInfo()
-        {
-            try
-            {
-                if (Session["TraineeID"] == null) return;
-
-                string traineeID = Session["TraineeID"].ToString();
-
-                //string query = @"SELECT 
-                //                    TM.TrainerID, 
-                //                    CASE WHEN TM.TrainerType='Internal' THEN E.EmpName ELSE TM.NameExternal END AS TrainerName,
-                //                    CASE WHEN TM.TrainerType='Internal' THEN E.EmpDesignation ELSE TM.DesignationExternal END AS Designation
-                //                FROM TrainerMaster TM 
-                //                LEFT JOIN EmpBasicMaster E ON TM.EmpID = E.EmpID 
-                //                WHERE TM.TrainerID = @TrainerID";
-
-                //SqlParameter[] param = new SqlParameter[] { new SqlParameter("@TrainerID", trainerID) };
-                //DataTable dt = obj.GetDataTable(query, param);
-
-                //if (dt.Rows.Count > 0)
-                //{
-                //    DataRow dr = dt.Rows[0];
-                //    lblTrainerName.Text = dr["TrainerName"]?.ToString() ?? "Trainer";
-                //    lblDesignation.Text = dr["Designation"].ToString();
-                //    // Session["TrainerName"] = lblTrainerName.Text;
-                //}
-                //else
-                //{
-                //    lblTrainerName.Text = "Trainer";
-                //}
-            }
-            catch
-            {
-                //lblTrainerName.Text = "Trainer";
+                Response.Redirect("~/Default.aspx");
+                return;
             }
         }
     }
