ea3dff9251bfc5109be1381756097a4d7e913557
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-21 15:00:30 +0530

    Add Close Training designer declaration

--- Training/Admin/ManageTraining.aspx.designer.cs
+++ Training/Admin/ManageTraining.aspx.designer.cs
@@ -14,6 +14,8 @@ namespace Training.Admin {
         protected global::System.Web.UI.WebControls.Button btnAssignHostel;
         protected global::System.Web.UI.WebControls.Button btnStartTraining;
         protected global::System.Web.UI.WebControls.Button btnAttendance;
+        
+        protected global::System.Web.UI.WebControls.Button btnCloseTraining;
         protected global::System.Web.UI.WebControls.Panel pnlHostelConfirmation;
         protected global::System.Web.UI.WebControls.Button btnHostelYes;
         protected global::System.Web.UI.WebControls.Button btnHostelNo;
